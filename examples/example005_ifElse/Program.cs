7304174a712d2d50115c51d8f601d92fe4054679
Katerina <kotia191@mail.ru>
2023-10-14 21:13:19 +0900

    005 введите имя юзер

---
+++ examples/example005_ifElse/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")    //ToLower() перевод в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
+
+
